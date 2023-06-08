2ddb8dfca4aa1ae12a10d80211ca19c119d68808
orewaee <71517526+orewaee@users.noreply.github.com>
2023-06-08 14:39:55 +0400

    Added web server

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+val ktorVersion: String by project
+val kotlinVersion: String by project
+
 plugins {
     application
 
@@ -5,6 +8,8 @@ plugins {
     kotlin("plugin.serialization") version "1.8.21"
 
     id("com.github.johnrengelman.shadow") version "7.1.2"
+
+    id("io.ktor.plugin") version "2.3.1"
 }
 
 group = "ru.orewaee"
@@ -16,6 +21,12 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+
+    implementation("io.ktor:ktor-server-cors-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
 }
 
 application.mainClass.set("MainKt")
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,49 @@
 import commands.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import plugins.configureHTTP
+import plugins.configureRouting
+import plugins.configureSerialization
 
 fun main() {
     Storage.load()
 
-    while (true) {
-        val arguments = readln().split(" ")
-
-        val command = when (arguments[0]) {
-            "all" -> All
-            "find" -> Find
-            "get" -> Get
-            "add" -> Add
-            "remove" -> Remove
-            "edit" -> Edit
-            "reload" -> Reload
-            "stop" -> Stop
-
-            else -> null
+    runBlocking {
+        launch(Dispatchers.Default) {
+            embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+                .start(wait = true)
         }
 
-        if (command == null) println("Command not found")
-        else command.execute(arguments.slice(1..arguments.lastIndex))
+        launch(Dispatchers.Default) {
+            while (true) {
+                val arguments = readln().split(" ")
+
+                val command = when (arguments[0]) {
+                    "all" -> All
+                    "find" -> Find
+                    "get" -> Get
+                    "add" -> Add
+                    "remove" -> Remove
+                    "edit" -> Edit
+                    "reload" -> Reload
+                    "stop" -> Stop
+
+                    else -> null
+                }
+
+                if (command == null) println("Command not found")
+                else command.execute(arguments.slice(1..arguments.lastIndex))
+            }
+        }
     }
+}
+
+fun Application.module() {
+    configureHTTP()
+    configureSerialization()
+    configureRouting()
 }
